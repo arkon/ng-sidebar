e7a7e3c1999f70d3f965467065944731b48651f0
Eugene Cheung <ey.cheung@outlook.com>
2016-09-29 12:04:59 -0400

    Import CommonModule instead of BrowserModule #12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import Sidebar, { SIDEBAR_POSITION } from './sidebar.component';
 import CloseSidebar from './close.directive';
@@ -8,7 +8,7 @@ export { SIDEBAR_POSITION };
 
 @NgModule({
   declarations: [Sidebar, CloseSidebar],
-  imports: [BrowserModule],
+  imports: [CommonModule],
   exports: [Sidebar, CloseSidebar]
 })
 export class SidebarModule {}
